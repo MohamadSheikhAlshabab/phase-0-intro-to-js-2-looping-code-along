3972b9c9ff7c546febf42198a07b4e33e73232b2
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-09-27 12:05:05 +0300

    Assigment Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+let arrayOfThanks = [];
+function writeCards( arrayOfNames , niceWord){
+    for (let name of arrayOfNames){
+        arrayOfThanks.push(`Thank you, ${name}, for the wonderful ${niceWord} gift!`);
+    }
+    return arrayOfThanks;
+}
+
+function countDown(number){
+    for (let count = number; count >= 0 ; count-- ){
+        console.log(count);
+    }
+}
